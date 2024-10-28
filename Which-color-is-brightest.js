d68ad1b78aa482b7e67579323a1342891b518b87
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-10-28 15:39:49 -0700

    Update Which-color-is-brightest.js

--- Which-color-is-brightest.js
+++ Which-color-is-brightest.js
@@ -22,3 +22,20 @@ Note: that both input and output should use upper case for characters A, B, C, D
 
 
 // Solution
+
+function brightest(colors){
+  let index = 0;
+  let maxValue = 0;
+  for(let i = 0; i < colors.length; i++) {
+    let color = colors[i];
+    let r = parseInt(color.slice(1,3),16);
+    let g = parseInt(color.slice(3,5),16);
+    let b = parseInt(color.slice(5,7),16);
+    let value = Math.max(r,g,b)
+    if (value > maxValue) {
+      maxValue = value
+      index = i
+    }
+  }
+  return colors[index]
+}
